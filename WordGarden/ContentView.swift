283f98c329539a9bf137525d0d19a3d7993d1255
Pavel Pacák <pavel.pacak@gmail.com>
2025-04-11 16:38:43 +0200

    Changes after lesson 2.3: Working with TextFields, Keyboards, FocusState

--- WordGarden/ContentView.swift
+++ WordGarden/ContentView.swift
@@ -16,6 +16,7 @@ struct ContentView: View {
     @State private var guessedLetter = ""
     @State private var imageName = "flower8"
     @State private var playAgainHidden = true
+    @FocusState private var textFieldIsFocused: Bool
     
     var body: some View {
         VStack {
@@ -55,18 +56,30 @@ struct ContentView: View {
                             RoundedRectangle(cornerRadius: 5)
                                 .stroke(.gray, lineWidth: 2)
                         }
+                        .keyboardType(.asciiCapable)
+                        .submitLabel(.done)
+                        .autocorrectionDisabled()
+                        .textInputAutocapitalization(.characters)
+                        .onChange(of: guessedLetter) {
+                            guessedLetter = guessedLetter.trimmingCharacters(in: .letters.inverted)
+                            guard let lastChar = guessedLetter.last else {
+                                return
+                            }
+                            guessedLetter = String(lastChar).uppercased()
+                        }
+                        .focused($textFieldIsFocused)
                     
                     Button("Guess a Letter:") {
                         //TODO: Guess a Letter button action here
-                        playAgainHidden = false
+                        textFieldIsFocused = false
                     }
                     .buttonStyle(.bordered)
                     .tint(.mint)
+                    .disabled(guessedLetter.isEmpty)
                 }
             } else {
                 Button("Another Word?") {
                     //TODO: Another Word Button Action Here
-                    playAgainHidden = true
                 }
                 .buttonStyle(.borderedProminent)
                 .tint(.mint)
@@ -77,6 +90,7 @@ struct ContentView: View {
             Image(imageName)
                 .resizable()
                 .scaledToFit()
+            
             
         }
         .ignoresSafeArea(edges: .bottom)
